8ca9992aaa21683ffbd8e7c7f65a6c05d3e6df19
Heropaster <heropasta@mail.ru>
2023-06-03 17:11:31 +0700

    немного переработал меню: Анимация появления меню при наведении на триггер

--- nav.js
+++ nav.js
@@ -1,7 +1,24 @@
-$(".menu li").on("mouseover touchstart", function() {
-    let idx = $(this).index();
-    $(".goo li").removeClass("hover");
-    $("nav li").eq(idx).addClass("hover");
-  }).on("mouseout touchend", function() {
-    $("nav li").removeClass("hover");
+  //Всплытие элементов меню
+  $(".menu li").on("mouseover touchstart", function() {
+      let idx = $(this).index();
+      $(".goo li").removeClass("hover");
+      $("nav li").eq(idx).addClass("hover");
+    }).on("mouseout touchend", function() {
+      $("nav li").removeClass("hover");
+    });
+  //Появление меню при наведении на триггер
+  let menuTimer;
+
+  $(".menu__trigger").on("mouseover touchstart", function() {
+    clearTimeout(menuTimer);
+    $(".menu__container").slideDown(250);
+    $(".menu__trigger").animate({ opacity: "0" }, 150);
+  });
+
+  $("body").on("mouseleave", function() {
+    menuTimer = setTimeout(function() {
+      $(".menu__container").slideUp(function() {
+        $(".menu__trigger").animate({ opacity: "1" }, 150);
+      });
+    }, 200);
   });
